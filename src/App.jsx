d44858b1d788bf4c475f72bd68b92da6ec3c7319
xdd <xdd@xdd.com>
2024-03-12 19:59:55 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="overlay"></div>
+      <div className="stars" aria-hidden="true"></div>
+      <div className="starts2" aria-hidden="true"></div>
+      <div className="stars3" aria-hidden="true"></div>
+      <main className="main">
+        <section className="contact">
+          <h1 className="title">Room Pals! .v2</h1>
+          <h2 className="sub-title">Site Under Construction</h2>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
